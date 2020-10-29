98925d5ef5ba6b3b426ea61f7134000687d4e99e
yumyumsuppe <b.vaethjunker@gmail.com>
2020-10-29 10:49:55 +0100

    initial commit

---
+++ inference-machine/src/main/java/com/htwg/dima/App.java
@@ -0,0 +1,13 @@
+package com.htwg.dima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
