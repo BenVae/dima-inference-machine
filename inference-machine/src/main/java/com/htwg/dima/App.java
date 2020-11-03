3cdbaa5b671e7e96c0f857470301f160b99666e0
yumyumsuppe <b.vaethjunker@gmail.com>
2020-11-03 10:57:49 +0100

    push to pat

--- inference-machine/src/main/java/com/htwg/dima/App.java
+++ inference-machine/src/main/java/com/htwg/dima/App.java
@@ -6,40 +6,39 @@ import aima.core.logic.fol.domain.FOLDomain;
 import aima.core.logic.fol.inference.InferenceResult;
 import aima.core.logic.fol.kb.FOLKnowledgeBase;
 import aima.core.logic.fol.parsing.ast.Constant;
-import aima.core.logic.fol.parsing.ast.Function;
 import aima.core.logic.fol.parsing.ast.Predicate;
-import aima.core.logic.fol.parsing.ast.Sentence;
 import aima.core.logic.fol.parsing.ast.Term;
-import aima.core.logic.fol.parsing.ast.Variable;
+import edu.princeton.cs.introcs.StdOut;
 
 public class App {
     public static void main(String[] args) {
-        FOLFCAsk folbcAsk = new FOLFCAsk();
+        FOLFCAsk folfcAsk = new FOLFCAsk();
 
         FOLDomain folDomain = new FOLDomain();
 
         folDomain.addFunction("American");
-        folDomain.addConstant("West");
         folDomain.addFunction("Missile");
-        folDomain.addConstant("M1");
-        folDomain.addPredicate("Owns");
-        folDomain.addConstant("Nono");
-        folDomain.addConstant("M1");
+
         folDomain.addPredicate("Enemy");
-        folDomain.addConstant("Nono");
+        folDomain.addPredicate("Owns");
+
         folDomain.addConstant("America");
-
-        ArrayList<Term> terms = new ArrayList<>();
-        terms.add(new Constant("West"));
-
-
-        Predicate function = new Predicate("Criminal", terms);
+        folDomain.addConstant("West");
+        folDomain.addConstant("Nono");
+        folDomain.addConstant("M1");
 
         FOLKnowledgeBase folKnowledgeBase = new FOLKnowledgeBase(folDomain);
 
-        folbcAsk.ask(folKnowledgeBase, function);
+        
+        ArrayList<Term> terms = new ArrayList<>();
+        terms.add(new Constant("West"));
+        Predicate predicate = new Predicate("Criminal", terms);
+        
 
-        folbcAsk.ask(folKnowledgeBase, function);
+        InferenceResult ask = folfcAsk.ask(folKnowledgeBase, predicate);
+        
+        
 
+        StdOut.print(ask.isTrue());
     }
 }
