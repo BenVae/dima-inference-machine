61689ccf42a08f8461753ee23da921f7fe262812
yumyumsuppe <b.vaethjunker@gmail.com>
2020-11-15 15:11:04 +0100

    Solve "West is a criminal" with FCAsk

--- inference-machine/src/main/java/com/htwg/dima/App.java
+++ inference-machine/src/main/java/com/htwg/dima/App.java
@@ -1,44 +1,54 @@
 package com.htwg.dima;
 
-import java.util.ArrayList;
-
 import aima.core.logic.fol.domain.FOLDomain;
+import aima.core.logic.fol.inference.FOLFCAsk;
 import aima.core.logic.fol.inference.InferenceResult;
+import aima.core.logic.fol.inference.InferenceResultPrinter;
 import aima.core.logic.fol.kb.FOLKnowledgeBase;
-import aima.core.logic.fol.parsing.ast.Constant;
-import aima.core.logic.fol.parsing.ast.Predicate;
-import aima.core.logic.fol.parsing.ast.Term;
+import aima.core.logic.fol.parsing.FOLParser;
+import aima.core.logic.fol.parsing.ast.Sentence;
 import edu.princeton.cs.introcs.StdOut;
 
 public class App {
     public static void main(String[] args) {
+
+        InferenceResult ask = folfcAskIsWestCiminal();
+        
+        StdOut.print(InferenceResultPrinter.printInferenceResult(ask));
+    }
+
+    static InferenceResult folfcAskIsWestCiminal(){
         FOLFCAsk folfcAsk = new FOLFCAsk();
 
         FOLDomain folDomain = new FOLDomain();
-
-        folDomain.addFunction("American");
-        folDomain.addFunction("Missile");
-
-        folDomain.addPredicate("Enemy");
-        folDomain.addPredicate("Owns");
-
-        folDomain.addConstant("America");
         folDomain.addConstant("West");
-        folDomain.addConstant("Nono");
+        folDomain.addConstant("America");
         folDomain.addConstant("M1");
+        folDomain.addConstant("Nono");
 
-        FOLKnowledgeBase folKnowledgeBase = new FOLKnowledgeBase(folDomain);
+        folDomain.addPredicate("American");
+        folDomain.addPredicate("Weapon");
+        folDomain.addPredicate("Sells");
+        folDomain.addPredicate("Hostile");
+        folDomain.addPredicate("Criminal");
+        folDomain.addPredicate("Missile");
+        folDomain.addPredicate("Owns");
+        folDomain.addPredicate("Enemy");
 
-        
-        ArrayList<Term> terms = new ArrayList<>();
-        terms.add(new Constant("West"));
-        Predicate predicate = new Predicate("Criminal", terms);
-        
+        FOLKnowledgeBase kb = new FOLKnowledgeBase(folDomain);
 
-        InferenceResult ask = folfcAsk.ask(folKnowledgeBase, predicate);
-        
-        
+        kb.tell("( (((American(x) AND Weapon(y)) AND Sells(x,y,z)) AND Hostile(z)) => Criminal(x))");
+        kb.tell("Owns(Nono, M1)");
+        kb.tell("Missile(M1)");
+        kb.tell("((Missile(x) AND Owns(Nono,x)) => Sells(West,x,Nono))");
+        kb.tell("(Missile(x) => Weapon(x))");
+        kb.tell("(Enemy(x,America) => Hostile(x))");
+        kb.tell("American(West)");
+        kb.tell("Enemy(Nono,America)");
 
-        StdOut.print(ask.isTrue());
+        FOLParser parser = new FOLParser(folDomain);
+        Sentence proofQuery = parser.parse("Criminal(West)");
+
+        return folfcAsk.ask(kb, proofQuery);
     }
 }
